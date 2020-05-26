dce1b35094bc4e13da8114436001c4c01136c264
Naomi <hello@naomi.dev>
2020-05-26 19:39:01 +0100

    Added nav skeleton

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
+    <AppNav />
     <nuxt />
   </div>
 </template>
+
+<script>
+import AppNav from '~/components/Nav.vue'
+
+export default {
+  components: { AppNav }
+}
+</script>
 
 <style lang="scss"></style>
