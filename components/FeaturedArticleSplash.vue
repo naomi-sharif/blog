b7e5f0153848e8e40923c444642f066cd31d5d9b
Naomi <hello@naomi.dev>
2020-10-04 19:35:32 +0100

    Updated styling for featured article

--- components/FeaturedArticleSplash.vue
+++ components/FeaturedArticleSplash.vue
@@ -1,17 +1,28 @@
 <template>
   <AppContainer>
-    <article class="featured-article-splash">
-      <AppLabel />
-      <div>
-        <AppMainHeading />
-        <AppBodyText>This will be the body text</AppBodyText>
+    <div class="featured-article-splash">
+      <div class="featured-article-splash__content">
+        <AppLabel />
+        <article>
+          <AppMainHeading />
+          <AppBodyText>This will be the body text</AppBodyText>
+        </article>
+        <a class="featured-article-splash__link" href="#">
+          <FaIcon
+            class="featured-article-splash__link-icon"
+            :icon="['fa', 'arrow-right']"
+        /></a>
       </div>
-      <a class="featured-article-splash__link" href="#">
-        <FaIcon
-          class="featured-article-splash__link-icon"
-          :icon="['fa', 'arrow-right']"
-      /></a>
-    </article>
+
+      <div class="featured-article-splash__visuals">
+        <AppIllustration />
+        <AppIllustration
+          :delay="-2.2"
+          class="featured-article-splash__visual-offset"
+        />
+        <AppIllustration :delay="-3.8" />
+      </div>
+    </div>
   </AppContainer>
 </template>
 
@@ -20,19 +31,39 @@ import AppContainer from '~/components/Container'
 import AppLabel from '~/components/Label.vue'
 import AppMainHeading from '~/components/MainHeading'
 import AppBodyText from '~/components/BodyText'
+import AppIllustration from '~/components/Illustration'
 
 export default {
-  components: { AppContainer, AppLabel, AppMainHeading, AppBodyText }
+  components: {
+    AppContainer,
+    AppLabel,
+    AppMainHeading,
+    AppBodyText,
+    AppIllustration
+  }
 }
 </script>
 
-<style lange="scss">
+<style lang="scss">
+@import '~/assets/scss/mixins';
+
 .featured-article-splash {
+  @include desktop {
+    display: flex;
+  }
+}
+
+.featured-article-splash__content {
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
   height: calc(100vh - 112px);
+  flex-basis: 60%;
+
+  @include desktop {
+    align-items: flex-start;
+  }
 }
 
 .featured-article-splash__link {
@@ -49,5 +80,22 @@ export default {
 .featured-article-splash__link-icon {
   color: var(--icons-color);
   font-size: 2.5rem;
+}
+
+.featured-article-splash__visuals {
+  display: none;
+
+  @include desktop {
+    display: unset;
+    flex-basis: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .featured-article-splash__visual-offset {
+    align-self: flex-end;
+  }
 }
 </style>
