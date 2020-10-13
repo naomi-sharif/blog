185a872033af5774c52f24ce5a0ac4923d9535bc
Naomi <hello@naomi.dev>
2020-10-13 17:17:19 +0100

    Upadated label styling

--- components/Label.vue
+++ components/Label.vue
@@ -3,13 +3,22 @@
 </template>
 
 <style lang="scss">
+@import '~/assets/scss/mixins';
 .label {
   background-color: var(--accent-global);
-  padding: 0.5rem 2rem;
+  padding: 0.5rem 1rem;
   color: var(--icons-color);
-  font-size: 20px;
+  font-size: 16px;
   display: inline-block;
   border-radius: 15px;
   margin: 0.5rem;
+  margin-left: 0;
+
+  @include desktop {
+    margin: 1rem;
+    margin-left: 0;
+    font-size: 20px;
+    padding: 0.5rem 2rem;
+  }
 }
 </style>
